03371932c1b47b4a09f71f20882db80bbf368b0b
Kate <bozhko2906872020@gmail.com>
2023-02-19 14:21:39 +0300

    app how to get an advice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+const [newAdvice, setAdvice] = useState("");
+
+useEffect(() => {
+  getAdvice();
+}, [])
+const getAdvice = async () => {
+const responce = await fetch('http://www.boredapi.com/api/activity/');
+const res = await responce.json();
+setAdvice(res.activity)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+       <div>
+      <p className='advice'>If you are bored just click here:</p>            
+     <button onClick={getAdvice}>GET ADVICE</button>
+    </div>
+  
+    <div className='title'>
+<h2>{newAdvice}</h2>
+  </div>
+    
     </div>
   );
 }
